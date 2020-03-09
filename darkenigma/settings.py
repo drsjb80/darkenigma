468214ee105a927f542c5fcf42fb3c8196a06a6a
drsjb80 <drsjb80@gmail.com>
2020-03-09 14:44:24 -0600

    Moving to elasticsearch.

--- darkenigma/settings.py
+++ darkenigma/settings.py
@@ -23,6 +23,16 @@ USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:7.0.1) Gecko/20100101 Firefox/7
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+ITEM_PIPELINES = [
+  'scrapyelasticsearch.ElasticSearchPipeline',
+]
+
+ELASTICSEARCH_SERVER = 'localhost'
+ELASTICSEARCH_PORT = 9200
+ELASTICSEARCH_INDEX = 'darkenigma'
+ELASTICSEARCH_TYPE = 'meetup'
+ELASTICSEARCH_UNIQ_KEY = 'url'
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
--- darkenigma/spiders/darkenigma.py
+++ darkenigma/spiders/darkenigma.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
-
-with open('phrases') as p:
-    phrases = [line.strip('\n') for line in p]
-
-with open('locations') as l:
-    locations = [line.strip('\n') for line in l]
+import datetime
+from elasticsearch import Elasticsearch
 
 count = 0
 
@@ -14,28 +10,30 @@ class DarkenignmaSpider(scrapy.Spider):
     name = 'darkenignma'
     # custom_settings = { 'DEPTH_LIMIT': 0 }
 
+    es = Elasticsearch()
+
     with open('URLs') as u:
         start_urls = [line.strip('\n') for line in u]
 
     def parse(self, response):
-        # print("Looking at " + response.url)
+        print("Looking at " + response.url)
         global count
         count += 1
         if (count % 10000) == 0:
             print(count, flush=True)
 
         for location in locations:
-            l = r'\b' + location + r'\b'
-            if re.search(l, response.text, re.IGNORECASE):
-                # print(location + ' found in body of ' + response.url, flush=True)
-                for phrase in phrases:
-                    p = r'\b' + phrase + r'\b'
-                    if re.search(p, response.text, re.IGNORECASE):
-                        print(phrase + ' and ' + location + ' found in ' + response.url, flush=True)
+            tosave = { 'url': response.url,
+                'text': response.text,
+                'timestamp': datetime.now(),
+            }
 
-                for href in response.css('a::attr(href)'):
-                    href = re.sub(r'#.*', '', response.urljoin(href.get()))
-                    if re.search('http:\/\/.*\.onion.*', href, re.IGNORECASE):
-                        # print('Following ' + href)
-                        yield response.follow(href, self.parse)
+            res = es.index(index="darkenigma", body=tosave)
+            print(res['result'])
+
+            for href in response.css('a::attr(href)'):
+                href = re.sub(r'#.*', '', response.urljoin(href.get()))
+                if re.search('https?:\/\/.*\.onion.*', href, re.IGNORECASE):
+                    print('Following ' + href)
+                    yield response.follow(href, self.parse)
 
